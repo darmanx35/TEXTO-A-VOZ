71c8e783bcb720c8b4880be19718d91cc9e7545a
darmanx35 <davidsalguerodavid1820@gmail.com>
2026-04-24 02:26:43 -0600

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        '3xl': '1.5rem',
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
